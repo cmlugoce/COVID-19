4adc43e170d5720d07795bdb98add11b5b29101e
Cristina Lugo <cristinalugo@Cristinas-MBP.lan>
2020-04-03 13:44:52 -0400

    add styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,15 +7,15 @@ const Header = () => {
   return (
     <header>
       <Container type="content">
-        <p>My Gatsby Site</p>
-        <ul>
+        <p>Cristina's COVID-19 Dashboard</p>
+        {/* <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/page-2/">Page 2</Link>
           </li>
-        </ul>
+        </ul> */}
       </Container>
     </header>
   );
